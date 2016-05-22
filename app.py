a19125457d0ae63bff5b0105aa25b43b7d54aeb4
Rob <robertchristopherdev11@gmail.com>
2016-05-22 00:50:48 -0400

    Create ad route

--- app.py
+++ app.py
@@ -1,16 +1,40 @@
 import itertools
 import operator
 import boto3
-from flask import Flask
-from flask.ext.restful import Api, Resource
+import uuid
+import json
+from flask import Flask, request, jsonify
 
 # Service resource
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('CustomerAd')
 # Init flask app
 app = Flask(__name__)
-api = Api(app)
 
+@app.route('/ad', methods=['POST'])
+def target():
+  body = request.form
+  name = body['name']
+  template = body['template']
+  acqusition_rate = body['acqusition_rate']
 
+  response = table.put_item(
+    TableName = "CustomerAd",
+    Item = {
+      "uuid": str(uuid.uuid4()),
+      "customers_reached": {"N": 0},
+      "customer_clicks": {"N": 0},
+      "name": {"S": name},
+      "customers_installed":{"N": 0},
+      "template": {"S": template},
+      "acqusition_rate": {"N": acqusition_rate},
+      "descriptors": {"SS": [
+        'string'
+      ]}
+    }
+  )
+
+  return jsonify(response), response['ResponseMetadata']['HTTPStatusCode']
+ 
 if __name__ == '__main__':
     app.run(debug=True)
